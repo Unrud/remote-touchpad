147a4b2874fc87b9a8dddace0005fb8785ae311c
Unrud <unrud@outlook.com>
2018-12-31 14:48:29 +0100

    fix license header

--- keysyms.generator.go
+++ keysyms.generator.go
@@ -5,7 +5,7 @@
  *
  *    This file is part of Remote-Touchpad.
  *
- *    Foobar is free software: you can redistribute it and/or modify
+ *    Remote-Touchpad is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
  *    the Free Software Foundation, either version 3 of the License, or
  *    (at your option) any later version.
@@ -16,7 +16,7 @@
  *    GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with Remote-Touchpad.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package main
